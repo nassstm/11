f2947767dfca014dde13a94811cbbe6f27559434
nassstm <112973667+nassstm@users.noreply.github.com>
2022-09-06 20:41:06 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Linq;
+
+//Задание 1
+
+namespace Mat
+{
+
+    internal class Program
+    {
+
+        private static void Main()
+        {
+            string s = "Slave";
+
+            var arr = s.ToCharArray();
+
+            var qu = arr.Distinct();
+
+            foreach (var z in qu)
+            {
+                Console.WriteLine("{0} повторяется {1} раз", z, s.Count(c => c == z));
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+}
